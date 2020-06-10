8e2d830006b38a57fd08c8f57a5f32f79976376b
David <dsi28@hotmail.com>
2020-06-09 23:36:52 -0500

    finish carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(function(){
+    $('.carousel').carousel( { interval: 2000 } );
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('i').hasClass('fa-pause')){
+            $('.carousel').carousel('pause');
+            $('#carouselButton').children('i').removeClass('fa-pause');
+            $('#carouselButton').children('i').addClass('fa-play');
+        }else{
+            $('.carousel').carousel('cycle');
+            $('#carouselButton').children('i').removeClass('fa-play');
+            $('#carouselButton').children('i').addClass('fa-pause');
+        }
+    });
     //checkScroll checks if nav bar's position changed and applies class to make it transparent
     function checkScroll(){
-        let startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+        let startY = $('.navbar').height();// * 2; //The point where the navbar changes in px
     
         if($(window).scrollTop() > startY){
             $('.navbar').addClass("scrolled");
